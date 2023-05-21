4f41043fcb44bc2a6098fbac3d892846bcf9d506
isaMuneretto <isamuneretto@gmail.com>
2023-05-21 12:18:41 -0300

    Data pagination

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
     try {
@@ -24,11 +24,54 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-    const news = await findAllService();
+    //para transformar o valor de limit e offset não pode ser uma constante
+    let {limit, offset} = req.query; 
+    //de string para number
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if(!limit) {
+        limit = 5;
+    }
+
+    if(!offset) {
+        offset = 0; //é de onde eu começo, nesse caso de 0 e depois pula de 5 em 5
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit; ;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null; //cria uma nova url
+
+    //typeof em js é para ver qual tipo de variável e vai ser uma string e tudo que envia no query params é uma string
+    //console.log(typeof limit, typeof offset); 
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null; 
+
     if (news.length === 0) { 
         return res.status(400).send({ message: "There are no registered news" });
     }
-    res.send(news)   //manda um obj
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map((item) => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            userName: item.user.username,
+            avatar: item.user.avatar,
+        })),
+    })   //manda um obj
 };
 
 export { create, findAll };
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,9 +2,16 @@ import News from "../models/News.js";
 
 const createService = (body) => News.create(body);
 
-const findAllService = () => News.find();
+//o findAll é quando busca no banco de dados. Aqui define vários parâmetros de busca
+//-1 ordena do último id criado (mais recente) até o primeiro id
+//skip começa de offset que é o zero e depois pula de 5 em 5 e limit que define quantos vai trazer
+//populate traz os dados 
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user"); 
+
+const countNews = () => News.countDocuments();
 
 export {
     createService,
-    findAllService
+    findAllService,
+    countNews
 };
